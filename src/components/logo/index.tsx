7316f87eba3d5c47509269b9bbaa26a13065cf7e
kanakuii <naomidao814@gmail.com>
2026-01-07 14:34:42 -0600

    added hover to logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link className="flex justify-center items-center w-full" href="/">
-            <img className="w-[25%]" src="/logo.png" alt="logo"/>
+        <Link className=" flex justify-center items-center w-full" href="/">
+            <img className="hover:scale-105 transition-transform duration-100 w-[25%]" src="/logo.png" alt="logo"/>
         </Link>
     );
 }
